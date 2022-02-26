bf82cffb5918153eb3f63178ba16fbceb8e36470
Grant Montgomery <grantm@rusondia.net>
2022-02-26 00:14:45 -0600

    try with params

--- src/routes/random/index.ts
+++ src/routes/random/index.ts
@@ -1,6 +1,6 @@
 import { getRandomWord } from '../../util/words';
 
-export async function get() {
+export async function get({ params }) {
     const answer = getRandomWord();
     return {
         body: {answer},
