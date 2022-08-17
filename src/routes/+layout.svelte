6beabe29f418af209172b6192608d98fcbafa119
Grant Montgomery <lettucebowler@gmail.com>
2022-08-16 21:24:21 -0500

    more assets

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import LetterPageContentContainer from '$lib/components/LetterPageContentContainer.svelte';
 	import { SvelteToast } from '@zerodevx/svelte-toast';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import smallFavicon from '$lib/assets/favicon-16x16.png';
+	import bigFavicon from '$lib/assets/favicon-32x32.png';
+	import appleTouchIcon from '$lib/assets/apple-touch-icon.png';
+	import safariPinnedTabIcon from '$lib/assets/safari-pinned-tab.svg';
 
 	import '$lib/assets/app.css';
 	import '$lib/assets/icomoon.css';
@@ -17,6 +21,13 @@
 		}
 	];
 </script>
+
+<svelte:head>
+	<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+	<link rel="icon" type="image/png" sizes="32x32" href={bigFavicon} />
+	<link rel="icon" type="image/png" sizes="16x16" href={smallFavicon} />
+	<link rel="mask-icon" href={safariPinnedTabIcon} color="#a3be8c" />
+</svelte:head>
 
 <div class="toast">
 	<SvelteToast />
