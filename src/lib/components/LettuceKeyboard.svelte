b2f83705efa93f9d654a601ffb6b2393d7efd9a6
Grant Montgomery <lettucebowler@gmail.com>
2022-08-18 19:29:07 -0500

    convert keyboard to tailwind

--- src/lib/components/LettuceKeyboard.svelte
+++ src/lib/components/LettuceKeyboard.svelte
@@ -6,71 +6,20 @@
 	};
 </script>
 
-<div class="board">
-	<div class="grow">
-		<div class="row">
-			{#each ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'] as key}
-				<div class="key">
-					<LettuceKey {key} status={keyStatuses[key]} on:letterTyped />
-				</div>
-			{/each}
+<div class="flex flex-col w-full gap-1 h-full justify-end">
+	{#each [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'], ['', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '', '', ''], ['', '', '', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'enter', 'delete']] as row}
+		<div class="flex w-full flex-col justify-center flex-auto">
+			<div class="grid gap-1 flex-auto grid-cols-[repeat(40,_0.25fr)]">
+				{#each row as key}
+					{#if key}
+						<div class="col-span-4 place-items-center grid">
+							<LettuceKey {key} status={keyStatuses[key]} on:letterTyped />
+						</div>
+					{:else}
+						<div />
+					{/if}
+				{/each}
+			</div>
 		</div>
-	</div>
-	<div class="grow">
-		<div class="row">
-			<div />
-			{#each ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'] as key}
-				<div class="key">
-					<LettuceKey {key} status={keyStatuses[key]} on:letterTyped />
-				</div>
-			{/each}
-			<div />
-			<div />
-			<div />
-		</div>
-	</div>
-	<div class="grow">
-		<div class="row">
-			<div />
-			<div />
-			<div />
-			{#each ['z', 'x', 'c', 'v', 'b', 'n', 'm', 'enter', 'delete'] as key}
-				<div class="key">
-					<LettuceKey {key} status={keyStatuses[key]} on:letterTyped />
-				</div>
-			{/each}
-		</div>
-	</div>
+	{/each}
 </div>
-
-<style>
-	.grow {
-		display: flex;
-		flex: 1 1 auto;
-		width: 100%;
-		flex-direction: column;
-		justify-content: center;
-	}
-
-	.board {
-		display: flex;
-		flex-direction: column;
-		width: 100%;
-		gap: 4px;
-		height: 100%;
-		justify-content: flex-end;
-	}
-
-	.row {
-		display: grid;
-		gap: 4px;
-		grid-template-columns: repeat(40, 0.25fr);
-		flex: 1 1 auto;
-	}
-
-	.key {
-		grid-column: span 4;
-		place-items: center;
-		display: grid;
-	}
-</style>
