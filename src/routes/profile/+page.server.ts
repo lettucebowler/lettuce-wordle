607db1174706d2cc0ab681c3828f74d07f73edbc
Grant Montgomery <lettucebowler@gmail.com>
2022-10-10 18:54:54 -0500

    bleh 37

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -9,28 +9,5 @@ export const load: import('./$types').PageServerLoad = async (event) => {
 	}
 
 	const results = await getGameResults(user.login, getGameNum());
-	console.log(results);
-	// const cookie = cookies.get('wordLettuceState') || '';
-	// let gameState = decodeState(cookie);
-	// const dailyWord = getDailyWord();
-	// const isStateForToday = gameState?.answer === dailyWord;
-	// if (!isStateForToday) {
-	// 	gameState = {
-	// 		answer: dailyWord,
-	// 		guesses: [],
-	// 		answers: []
-	// 	};
-	// }
-
-	// cookies.set('wordLettuceState', encodeState(gameState), {
-	// 	httpOnly: false,
-	// 	path: '/',
-	// 	maxAge: 86400,
-	// 	secure: false
-	// });
-
-	// return {
-	// 	state: gameState
-	// };
-	return results;
+	return { gameResults: results };
 };
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,1 +1,7 @@
+<script lang="ts">
+	export let data: import('./$types').PageData;
+
+	$: gameResults = data.gameresults;
+</script>
+
 <h1 class="w-full text-center text-3xl text-snow-300">Nothing to see here</h1>
