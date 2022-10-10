f2e0e9d315d9306095ec05a807cbac1e9bf02048
Grant Montgomery <lettucebowler@gmail.com>
2022-10-10 00:43:55 -0500

    don't write to redis on every request

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,16 +4,17 @@ import { getUser } from '$lib/util/auth';
 
 export const handle: import('@sveltejs/kit').Handle = async ({ event, resolve }) => {
 	const session = event.cookies.get(SESSION_COOKIE_NAME) || '';
-
 	if (session) {
+		let refresh = false;
 		// try and grab caches profile
 		let user = await getProfile(session);
 		if (!user.login) {
 			// grab from origin if cache miss
 			user = await getUser(session);
+			refresh = true;
 		}
 		// cache profile again if found
-		if (user.login) {
+		if (refresh) {
 			await stashProfile(session, user);
 		}
 		// clear session cookie if no profile is available with it any longer
