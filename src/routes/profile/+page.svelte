00f48b238be297731949506961a93ac2e86b2436
Grant Montgomery <lettucebowler@gmail.com>
2022-10-10 19:05:54 -0500

    bleh 38

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+	import { getGameResults } from '$lib/client/planetscale';
+
 	export let data: import('./$types').PageData;
 
-	$: gameResults = data.gameresults;
+	$: gameResults = data.gameResults;
+	$: console.log(gameResults);
 </script>
 
 <h1 class="w-full text-center text-3xl text-snow-300">Nothing to see here</h1>
+
+{#each gameResults as gameResult, i (gameResult.gamenum)}
+	<div>
+		<h2>{gameResult.gamenum}</h2>
+		<p>{gameResult.answers}</p>
+	</div>
+{/each}
