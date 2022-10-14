b6a4353f2f6a2cfc45f36e7856006c021b15cd3d
Grant Montgomery <lettucebowler@gmail.com>
2022-10-14 11:53:50 -0500

    use adapter-cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,9 +10,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter({
-			out: 'build'
-		}),
+		adapter: adapter(),
 		inlineStyleThreshold: 2048
 	}
 };
