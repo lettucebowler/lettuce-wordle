8b764d96340e5360b5c83ca0f17e94a8a9f9d592
Grant Montgomery <lettucebowler@gmail.com>
2022-10-17 01:25:12 -0500

    fix ranking page so links work when js is not working

--- src/routes/rankings/+page.svelte
+++ src/routes/rankings/+page.svelte
@@ -18,35 +18,35 @@
 		</P>
 	</div>
 	<div class="h-8" />
-	<table class="grid w-full max-w-2xl gap-2 text-snow-300">
+	<div class="grid w-full max-w-2xl gap-2 text-snow-300">
 		<thead>
-			<tr class="grid w-full grid-cols-3 rounded-2xl bg-polar-400 p-2 text-xl font-medium">
-				<td class="text-left">Rank</td>
-				<td class="text-left">User</td>
-				<td class="text-right">score</td>
-			</tr>
+			<div class="grid w-full grid-cols-3 rounded-2xl bg-polar-400 p-2 text-xl font-medium">
+				<div class="text-left">Rank</div>
+				<div class="text-left">User</div>
+				<div class="text-right">score</div>
+			</div>
 		</thead>
-		<tbody class="grid gap-2">
+		<div class="grid gap-2">
 			{#each scores as score, i (i)}
 				{@const position = scores.filter((s) => s.score > score.score).length + 1}
 				<a href={`/profile/${score.user}`}>
-					<tr
+					<div
 						class="grid w-full grid-cols-3 items-center rounded-2xl p-2 text-xl font-medium"
 						class:bg-polar-300={i % 2 == 1}
 						class:bg-polar-200={i % 2 == 0}
 					>
-						<td class="grid items-center text-left text-snow-300"
-							>{#if position === 1}
+						<div class="grid items-center text-left text-snow-300">
+							{#if position === 1}
 								<Icon name="trophy" size={8} />
 							{:else}
 								#{position}
-							{/if}</td
-						>
-						<td class="text-left">{score.user}</td>
-						<td class="text-right">{score.score}</td>
-					</tr></a
+							{/if}
+						</div>
+						<div class="text-left">{score.user}</div>
+						<div class="text-right">{score.score}</div>
+					</div></a
 				>
 			{/each}
-		</tbody>
-	</table>
+		</div>
+	</div>
 </div>
