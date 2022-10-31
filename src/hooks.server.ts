fe3b5e264fbf8b83545c30f39267f3d97bce8814
Grant Montgomery <lettucebowler@gmail.com>
2022-10-31 17:34:04 -0500

    update to new kit version, remove workaround in hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -42,11 +42,11 @@ export const handle: import('@sveltejs/kit').Handle = async ({ event, resolve })
 	return response;
 };
 
-export const handleFetch: import('@sveltejs/kit').HandleFetch = async (event) => {
-	if (event.request.url.startsWith('https://github.com')) {
-		// Workaround: https://github.com/sveltejs/kit/issues/6608
-		event.request.headers.set('origin', event.request.url);
-	}
+// export const handleFetch: import('@sveltejs/kit').HandleFetch = async (event) => {
+// 	if (event.request.url.startsWith('https://github.com')) {
+// 		// Workaround: https://github.com/sveltejs/kit/issues/6608
+// 		event.request.headers.set('origin', event.request.url);
+// 	}
 
-	return fetch(event.request);
-};
+// 	return fetch(event.request);
+// };
