ad2e90df1f52e14e6dffd63d79ba4070a05de681
Grant Montgomery <lettucebowler@gmail.com>
2022-11-02 23:10:22 -0500

    max w to md breakpoint

--- src/lib/components/LetterPageContentContainer.svelte
+++ src/lib/components/LetterPageContentContainer.svelte
@@ -1,5 +1,5 @@
 <div class="absolute top-0 bottom-0 right-0 left-0 flex justify-center bg-polar-100">
-	<div class="relative bottom-2 top-0 flex w-full max-w-[800px] flex-col gap-2 p-1 pt-0">
+	<div class="relative bottom-2 top-0 flex w-full max-w-screen-md flex-col gap-2 p-1 pt-0">
 		<slot />
 	</div>
 </div>
