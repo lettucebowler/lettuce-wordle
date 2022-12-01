9adc88332edb7312a1ab8519c09e46c1d1a8e871
Grant Montgomery <lettucebowler@gmail.com>
2022-11-30 20:32:08 -0600

    bleh a trillion

--- src/lib/client/workers-kv.ts
+++ src/lib/client/workers-kv.ts
@@ -1,30 +1,30 @@
-import { fetcher } from 'itty-fetcher';
-import { KV_TOKEN, KV_HOST } from '$env/static/private';
+// import { fetcher } from 'itty-fetcher';
+// import { KV_TOKEN, KV_HOST } from '$env/static/private';
 
-const workersKV = fetcher({
-	base: KV_HOST,
-	transformRequest(req) {
-		req.headers['Authorization'] = `Bearer ${KV_TOKEN}`;
-		return req;
-	}
-});
+// const workersKV = fetcher({
+// 	base: KV_HOST,
+// 	transformRequest(req) {
+// 		req.headers['Authorization'] = `Bearer ${KV_TOKEN}`;
+// 		return req;
+// 	}
+// });
 
-export const get = async (key: string) => {
-	const before = new Date().getTime();
-	let data = {};
-	try {
-		data = JSON.parse(await workersKV.post('/get', { key }));
-	} catch (e) {
-		console.log(e);
-	}
-	const after = new Date().getTime();
-	console.log('load from KV', after - before);
-	return data;
-};
+// export const get = async (key: string) => {
+// 	const before = new Date().getTime();
+// 	let data = {};
+// 	try {
+// 		data = JSON.parse(await workersKV.post('/get', { key }));
+// 	} catch (e) {
+// 		console.log(e);
+// 	}
+// 	const after = new Date().getTime();
+// 	console.log('load from KV', after - before);
+// 	return data;
+// };
 
-export const set = async (key: string, value: any) => {
-	const data = await workersKV.post('/set', { key, value });
-	return data;
-};
+// export const set = async (key: string, value: any) => {
+// 	const data = await workersKV.post('/set', { key, value });
+// 	return data;
+// };
 
-export const getUserKV = (key: string) => get(key) as Promise<{ login?: string; avatar?: string }>;
+// export const getUserKV = (key: string) => get(key) as Promise<{ login?: string; avatar?: string }>;
