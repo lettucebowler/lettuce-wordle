1e04d502d165c8fde53e00785d2e982373ad0585
Grant Montgomery <lettucebowler@gmail.com>
2022-11-30 20:41:52 -0600

    bleh

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,14 @@ const config = {
 		preprocess({
 			postcss: true
 		})
-	]
+	],
+
+	kit: {
+		adapter: adapter({
+			out: 'build'
+		}),
+		inlineStyleThreshold: 2048
+	}
 };
 
 export default config;
