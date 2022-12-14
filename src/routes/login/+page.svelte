4df9ceed803ded90e03923ed746f9ffafe15efe4
Grant Montgomery <lettucebowler@gmail.com>
2022-12-14 16:33:15 -0600

    wip on @auth/core

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { signIn, signOut } from '@auth/sveltekit/client';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+    onMount(() => {
+        console.log('login');
+        signIn('github')
+    });
+</script>
--- src/routes/login/+server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-const ghAuthURL = 'https://github.com/login/oauth/authorize';
-import { CLIENT_ID } from '$env/static/private';
-
-export async function GET() {
-	const sessionId = '1234';
-	throw redirect(307, `${ghAuthURL}?client_id=${CLIENT_ID}&state=${sessionId}`);
-}
