e5b2cfd622c38eb8147c1b6e81699daa084ade5a
Grant Montgomery <lettucebowler@gmail.com>
2023-02-04 15:44:06 -0600

    switch up color pallette a bit

--- src/lib/components/LettuceAvatar.svelte
+++ src/lib/components/LettuceAvatar.svelte
@@ -5,7 +5,7 @@
 	export let size = 40;
 	export let square = true;
 
-	export let colors = ['#D08770', '#EBCB8B', '#A3BE8C', '#5E81AC', '#485164'];
+	export let colors = ['#BF616A', '#D08770', '#EBCB8B', '#A3BE8C', '#B48EAD', '#5E81AC', '#485164'];
 </script>
 
 <AvatarBeam {size} {square} {name} {colors} />
