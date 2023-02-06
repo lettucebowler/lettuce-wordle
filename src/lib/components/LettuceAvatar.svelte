21e0d8bbee4f8f0677671b4d7bdcc3dd986e551b
Grant Montgomery <lettucebowler@gmail.com>
2023-02-05 18:09:39 -0600

    use source api instead of component

--- src/lib/components/LettuceAvatar.svelte
+++ src/lib/components/LettuceAvatar.svelte
@@ -1,11 +1,22 @@
 <script>
-	import { AvatarBeam } from 'svelte-boring-avatars';
-
 	export let name = '';
 	export let size = 40;
 	export let square = true;
 
-	export let colors = ['#BF616A', '#D08770', '#EBCB8B', '#A3BE8C', '#B48EAD', '#5E81AC', '#485164'];
+	export let colors = ['D08770', 'EBCB8B', 'A3BE8C', '8FBCBB', '4C566A'];
+
+	import { PUBLIC_AVATAR_HOST } from '$env/static/public';
+
+	const host = PUBLIC_AVATAR_HOST ? PUBLIC_AVATAR_HOST : 'https://source.boringavatars.com';
 </script>
 
-<AvatarBeam {size} {square} {name} {colors} />
+<!-- <AvatarBeam {size} {square} {name} {colors} /> -->
+
+<img
+	style={`width:${size}px;height:${size}px`}
+	src={`${host}/beam/${size}/${name}?${new URLSearchParams({
+		square: square.toString(),
+		colors: colors.join(',')
+	}).toString()}`}
+	alt=""
+/>
