0e5e26ede43ec0c5dea2fa23c0f2be3ce6192667
Grant Montgomery <lettucebowler@gmail.com>
2023-03-04 16:36:29 -0600

    bleh

--- src/lib/client/apiWordlettuce.ts
+++ src/lib/client/apiWordlettuce.ts
@@ -5,29 +5,33 @@ import type { GameResult, LeaderboardResults } from '$lib/types/gameresult';
 import type { UserRecord } from '$lib/types/auth';
 
 const apiWordlettuce = fetcher({
-	base: `${API_WORDLETTUCE_HOST}/api`,
+	base: `${API_WORDLETTUCE_HOST}`,
 	transformRequest(req) {
 		req.headers['Authorization'] = `Bearer ${API_WORDLETTUCE_TOKEN}`;
 		return req;
 	}
 });
 
-export const getGameResults = async (user: string, count: number) => {
-	const gameResults = await apiWordlettuce.get(`/gameresults/${user}`, { count });
-	return gameResults as GameResult[];
+export const getGameResults = async (user: string, count: number, offset = 0) => {
+	const { results } = (await apiWordlettuce.get(`/users/${user}/gameresults`, {
+		count,
+		offset
+	})) as { results: GameResult[] };
+	return results as GameResult[];
 };
 
 export const getLeaderBoardResults = async (gamenum: number) => {
-	const leaderboardResults = await apiWordlettuce.get('/gameresults/leaderboard', { gamenum });
+	const leaderboardResults = await apiWordlettuce.get('/api/gameresults/leaderboard', { gamenum });
 	return leaderboardResults as LeaderboardResults[];
 };
 
 export const saveGameResults = async (gameresult: GameResult) => {
-	const results = await apiWordlettuce.post('/gameresults', gameresult);
+	const { username, gamenum, ...rest } = gameresult;
+	const results = await apiWordlettuce.put(`/users/${username}/gameresults/${gamenum}`, rest);
 	return results;
 };
 
 export const upsertUser = async (user: UserRecord) => {
-	const results = await apiWordlettuce.post('/user', user);
+	const results = await apiWordlettuce.post('/users', user);
 	return results;
 };
