3bdcee62b05456f9ccdfc4805b2ec137c0e1eb36
Grant Montgomery <lettucebowler@gmail.com>
2023-03-23 23:45:15 -0500

    update deps

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -100,7 +100,7 @@
 					viewBox="0 0 24 24"
 					stroke-width="1.5"
 					stroke="currentColor"
-					class="box-border aspect-square h-full "
+					class="box-border aspect-square h-full"
 				>
 					<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 				</svg>
