9c32166cbf4c9b5950bdcbe8eb5fc9be68d58e7c
Grant Montgomery <lettucebowler@gmail.com>
2023-10-13 00:24:17 -0500

    fix dicebear url, apparently it decided it doesn't like url encoded commas anymore

--- src/lib/components/LettuceAvatar.svelte
+++ src/lib/components/LettuceAvatar.svelte
@@ -3,10 +3,7 @@
 	export let size = 40;
 	const backgroundColors = ['BF616A', 'D08770', 'EBCB8B', 'A3BE8C', 'B48EAD', '88C0D0', '81A1C1'];
 
-	const searchParams = new URLSearchParams({
-		seed: name
-		// backgroundColor: backgroundColors,
-	});
+	const searchParams = new URLSearchParams();
 	backgroundColors.forEach((bg) => {
 		searchParams.append('backgroundColor', bg);
 	});
@@ -15,6 +12,6 @@
 <img
 	style={`width:${size}px;height:${size}px`}
 	class="pointer-events-none inline"
-	src={`https://api.dicebear.com/7.x/bottts-neutral/svg?${searchParams}`}
+	src={`https://api.dicebear.com/7.x/bottts-neutral/svg?${searchParams}&seed=${name}`}
 	alt=""
 />
