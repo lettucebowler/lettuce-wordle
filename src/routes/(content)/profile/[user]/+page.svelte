552d3415f83eb263608c2a9d8ca6aca509d88a9b
Grant Montgomery <lettucebowler@gmail.com>
2024-02-02 03:03:23 -0600

    blug

--- src/routes/(content)/profile/[user]/+page.svelte
+++ src/routes/(content)/profile/[user]/+page.svelte
@@ -2,60 +2,43 @@
 	import { infiniteScrollAction } from './actions.js';
 	import LettuceAvatar from '$lib/components/LettuceAvatar.svelte';
 	import { fetcher } from 'itty-fetcher';
+	import { page } from '$app/stores';
 	import { browser } from '$app/environment';
-	import { createInfiniteQuery } from '@tanstack/svelte-query';
 
 	export let data;
 
 	let fetchMore = data.more;
-	async function fetchGames({ pageParam = 1 } = {}) {
-		const wordlettuce = fetcher({
-			base: `${window.location.origin}/api/v1`
-		});
+	async function getNextBatch() {
+		if (!fetchMore) {
+			return;
+		}
 		const searchParams = new URLSearchParams({
-			limit: '30',
-			offset: `${(pageParam - 1) * 30}`,
+			count: '30',
+			offset: `${data.results.length + Number($page.url.searchParams.get('offset') || 0)}`,
 			user: data.user
 		});
-		return wordlettuce.get<{
-			results: { answers: string; gameNum: number }[];
-			limit: number;
-			offset: number;
-			more: boolean;
-		}>('/game-results', searchParams);
-	}
-
-	const query = createInfiniteQuery({
-		queryKey: ['game-results'],
-		queryFn: ({ pageParam }) => fetchGames({ pageParam }),
-		initialPageParam: data.offsets.next / 30 + 1,
-		getNextPageParam: (lastPage) => {
-			if (lastPage.more) {
-				return lastPage.offset / 30 + 2;
-			}
-			fetchMore = false;
-			return undefined;
-		},
-		refetchOnMount: false,
-		initialData: {
-			pages: [
-				{
-					results: data.results,
-					more: data.more,
-					offset: data.offsets.current,
-					limit: 30
-				}
-			],
-			pageParams: [1]
+		if ($page.url.searchParams.get('dbProvider')) {
+			searchParams.set('dbProvider', $page.url.searchParams.get('dbProvider') || '');
 		}
-	});
+		const oldLength = data.results.length;
+		const fetchResult = (await fetcher().get(`/api/v1/game-results`, searchParams)) as {
+			totalCount: number;
+			results: { answers: string; gameNum: number }[];
+		};
+		if (fetchResult?.results) {
+			data.results = data.results.concat(fetchResult.results);
+		}
+		const newLength = data.results.length;
+		if (newLength - oldLength < 30) {
+			fetchMore = false;
+		}
+	}
 </script>
 
 <svelte:body
 	use:infiniteScrollAction={{
 		distance: 100,
-		delay: 100,
-		cb: () => $query.fetchNextPage(),
+		cb: getNextBatch,
 		immediate: false,
 		disabled: !fetchMore
 	}}
@@ -75,37 +58,33 @@
 	<h1 class="text-center text-3xl font-bold text-snow-300">Play History</h1>
 
 	<div class="grid w-full grid-cols-2 gap-2 sm:grid-cols-3">
-		{#if $query.data}
-			{#each $query.data.pages ?? [] as results (results)}
-				{#each results.results as result (result)}
-					{@const answers = result.answers}
-					<div
-						class="flex w-full flex-[1_1_200px] flex-col gap-2 rounded-2xl border-4 border-solid border-charade-700 p-2"
+		{#each data.results.sort((a, b) => b.gameNum - a.gameNum) as gameResult (gameResult.gameNum)}
+			{@const answers = gameResult.answers}
+			<div
+				class="flex w-full flex-[1_1_200px] flex-col gap-2 rounded-2xl border-4 border-solid border-charade-700 p-2"
+			>
+				<h2 class="flex justify-between text-center text-xl font-medium text-snow-300">
+					<span class="text-left">#{gameResult.gameNum}</span><span class="text-right"
+						>{1 + 6 - gameResult.answers.length / 5} pts</span
 					>
-						<h2 class="flex justify-between text-center text-xl font-medium text-snow-300">
-							<span class="text-left">#{result.gameNum}</span><span class="text-right"
-								>{1 + 6 - result.answers.length / 5} pts</span
-							>
-						</h2>
-						<div class="grid grid-cols-5 gap-1">
-							{#each [...Array(30).keys()] as i}
-								{@const answer = answers.charAt(i) || '_'}
-								<div
-									class="box-border grid aspect-square w-full grid-rows-3 rounded-lg text-center text-2xl font-bold text-snow-300 sm:text-3xl"
-									class:border-charade-700={answer === '_'}
-									class:border-2={answer === '_'}
-									class:border-solid={answer === '_'}
-									class:bg-charade-900={answer === '_'}
-									class:bg-aurora-400={answer === 'x'}
-									class:bg-aurora-300={answer === 'c'}
-									class:bg-charade-700={answer === 'i'}
-								/>
-							{/each}
-						</div>
-					</div>
-				{/each}
-			{/each}
-		{/if}
+				</h2>
+				<div class="grid grid-cols-5 gap-1">
+					{#each [...Array(30).keys()] as i}
+						{@const answer = answers.charAt(i) || '_'}
+						<div
+							class="box-border grid aspect-square w-full grid-rows-3 rounded-lg text-center text-2xl font-bold text-snow-300 sm:text-3xl"
+							class:border-charade-700={answer === '_'}
+							class:border-2={answer === '_'}
+							class:border-solid={answer === '_'}
+							class:bg-charade-900={answer === '_'}
+							class:bg-aurora-400={answer === 'x'}
+							class:bg-aurora-300={answer === 'c'}
+							class:bg-charade-700={answer === 'i'}
+						/>
+					{/each}
+				</div>
+			</div>
+		{/each}
 	</div>
 	{#if !data.results.length}
 		<p class="rounded-xl p-2 text-center text-center text-xl text-snow-300">
