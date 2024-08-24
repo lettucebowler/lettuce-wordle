cf55a14e2ebc01851ce621db8fba171550d4878d
Grant Montgomery <lettucebowler@gmail.com>
2024-08-24 15:06:53 -0500

    fix gamenum and update deps

--- src/lib/util/words.ts
+++ src/lib/util/words.ts
@@ -2,7 +2,6 @@ export function getGameNum() {
 	const msInADay = 1000 * 60 * 60 * 24;
 	const initial = new Date(1643673600000);
 	const now = new Date();
-	return 932;
 	return Math.floor((now.getTime() - initial.getTime()) / msInADay);
 }
 
