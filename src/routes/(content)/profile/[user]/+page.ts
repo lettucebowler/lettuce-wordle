b23dae69885adf94399e53ec4423f49e60fb5b64
Grant Montgomery <lettucebowler@gmail.com>
2024-10-09 22:07:17 -0500

    bleh

--- src/routes/(content)/profile/[user]/+page.ts
+++ src/routes/(content)/profile/[user]/+page.ts
@@ -9,8 +9,8 @@ export async function load(event) {
 	const { user } = event.params;
 	const searchParams = event.url.searchParams;
 	const page = Number(searchParams.get('page')) || 1;
-
-	const { results, more } = await fetcher({ fetch: event.fetch }).get<{
+	const apiWordlettuce = fetcher({ fetch: event.fetch });
+	const { results, more } = await apiWordlettuce.get<{
 		results: GameResult[];
 		more: boolean;
 	}>('/api/v1/game-results', { user, page });
