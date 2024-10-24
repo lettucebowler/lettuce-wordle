c006b0cbdac465d5cd739dfaa40c23e176cb4864
Grant Montgomery <lettucebowler@gmail.com>
2024-10-24 17:23:21 -0500

    WIP

---
+++ src/routes/(content)/profile/[user]/+page.server.ts
@@ -0,0 +1,32 @@
+import { createWordlettuceBetaDao } from '$lib/dao/wordlettuce-beta.server.js';
+import { fetcher } from 'itty-fetcher';
+
+type GameResult = {
+	gameNum: number;
+	answers: string;
+};
+
+export async function load(event) {
+	const parentData = await event.parent();
+	const { user } = event.params;
+	const searchParams = event.url.searchParams;
+	const startParam = Number(searchParams.get('start')) || parentData.gameNum;
+	const { getNextPageAfter } = createWordlettuceBetaDao();
+	const results = await getNextPageAfter({ username: user, limit: 30, start: startParam });
+	// const apiWordlettuce = fetcher({ fetch: event.fetch });
+	// const { results, start, next } = await apiWordlettuce.get<{
+	// 	results: GameResult[];
+	// 	start: number;
+	// 	next: number | null;
+	// }>('/api/v1/game-results', { user, start: startParam });
+	// event.setHeaders({
+	// 	'Cache-Control': 'max-age=300'
+	// });
+	return {
+		user,
+		start: startParam,
+		next: results.at(-1)?.gameNum ?? null,
+		results: results.slice(0, 30),
+		...parentData
+	};
+}
