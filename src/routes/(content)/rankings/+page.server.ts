5760f2eedb7709b103bd6dcdb8393208b4d8624b
Grant Montgomery <lettucebowler@gmail.com>
2024-10-26 14:22:56 -0500

    bleh

--- src/routes/(content)/rankings/+page.server.ts
+++ src/routes/(content)/rankings/+page.server.ts
@@ -1,14 +1,14 @@
 // import { createWordlettuceBetaDao } from '$lib/dao/wordlettuce-beta.server';
-import { createApiWordlettuceClient } from '$lib/client/api-wordlettuce.client.js';
+import { createApiWordlettuceClient } from '$lib/client/api-wordlettuce.server.js';
 
 export async function load(event) {
 	// const { getRankings } = createWordlettuceBetaDao();
 	const apiWordlettuce = createApiWordlettuceClient(event);
 	const rankings = await apiWordlettuce.getRankings();
 
-	event.setHeaders({
-		'Cache-Control': 'public,max-age=60'
-	});
+	// event.setHeaders({
+	// 	'Cache-Control': 'public,max-age=60'
+	// });
 
 	return {
 		rankings
