df36756108990021bcc0caf32dce4e66f88b1ce3
Grant Montgomery <lettucebowler@gmail.com>
2024-10-30 11:27:02 -0500

    update deps and minor refactor

--- src/lib/client/api-wordlettuce.server.ts
+++ src/lib/client/api-wordlettuce.server.ts
@@ -1,24 +1,19 @@
 import { fetcher } from 'itty-fetcher';
 import { API_WORDLETTUCE_TOKEN } from '$env/static/private';
 import { PUBLIC_API_WORDLETTUCE_HOST } from '$env/static/public';
-
 import { error as svelteError, type RequestEvent } from '@sveltejs/kit';
 
-export function createApiWordLettuceFetcher(event: RequestEvent) {
-	return fetcher({
+export function createApiWordlettuceClient(event: RequestEvent) {
+	const api = fetcher({
 		fetch: event.fetch,
 		base: PUBLIC_API_WORDLETTUCE_HOST,
 		headers: {
 			Authorization: `Bearer ${API_WORDLETTUCE_TOKEN}`
 		}
 	});
-}
-
-export function createApiWordlettuceClient(event: RequestEvent) {
-	const wordlettuce = createApiWordLettuceFetcher(event);
 
 	async function upsertUser({ id, login }: { id: number; login: string }) {
-		const { error } = await wordlettuce
+		const { error } = await api
 			.put(`/v1/users/${id}`, { username: login })
 			.then((data) => ({ data, error: undefined }))
 			.catch((error) => ({ error, data: undefined }));
@@ -36,7 +31,7 @@ export function createApiWordlettuceClient(event: RequestEvent) {
 		gameNum: number;
 		answers: string;
 	}) {
-		const { data, error } = await wordlettuce
+		const { data, error } = await api
 			.post<{ gameNum: number; userId: string; answers: string; attempts: number }>(
 				'/v1/game-results',
 				{ userId, gameNum, answers }
@@ -58,7 +53,7 @@ export function createApiWordlettuceClient(event: RequestEvent) {
 		limit?: number;
 		start: number;
 	}) {
-		const { data, error } = await wordlettuce
+		const { data, error } = await api
 			.get<{
 				limit: number;
 				start: number;
@@ -74,11 +69,12 @@ export function createApiWordlettuceClient(event: RequestEvent) {
 	}
 
 	async function getRankings() {
-		const { data, error } = await wordlettuce
+		const { data, error } = await api
 			.get<{ rankings: Array<{ user: string; games: number; score: number }> }>('/v2/rankings')
 			.then((data) => ({ data, error: undefined }))
 			.catch((error) => ({ error, data: undefined }));
 		if (error || !data) {
+			console.log(error);
 			throw svelteError(500, error);
 		}
 		return data.rankings;
