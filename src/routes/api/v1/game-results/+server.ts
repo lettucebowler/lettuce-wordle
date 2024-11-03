92ace0cad3ae30e060af6c8bce4f8b24c18784b3
Grant Montgomery <lettucebowler@gmail.com>
2024-11-03 02:11:20 -0600

    bleh

--- src/routes/api/v1/game-results/+server.ts
+++ src/routes/api/v1/game-results/+server.ts
@@ -1,8 +1,8 @@
 import * as v from 'valibot';
 import { error, json } from '@sveltejs/kit';
-import { createWordlettuceBetaDao } from '$lib/dao/wordlettuce-beta.server.js';
+// import { createWordlettuceBetaDao } from '$lib/dao/wordlettuce-beta.server.js';
 import { getGameNum } from '$lib/util/words.js';
-// import { createApiWordlettuceClient } from '$lib/client/api-wordlettuce.server.js';
+import { createApiWordlettuceClient } from '$lib/client/api-wordlettuce.server.js';
 
 const EventToObjectSchema = v.pipe(
 	v.object({
@@ -41,13 +41,13 @@ export async function GET(event) {
 		error(400, 'Bad request');
 	}
 	const { user, start } = requestParseResult.output;
-	const db = createWordlettuceBetaDao(event);
-	const { results, next, limit } = await db.getNextPageAfter({
-		username: user,
-		start
-	});
-	// const api = createApiWordlettuceClient(event);
-	// const { results, next, limit } = await api.getNextPageAfter({ username: user, start });
+	// const db = createWordlettuceBetaDao(event);
+	// const { results, next, limit } = await db.getNextPageAfter({
+	// 	username: user,
+	// 	start
+	// });
+	const api = createApiWordlettuceClient(event);
+	const { results, next, limit } = await api.getNextPageAfter({ username: user, start });
 
 	// event.setHeaders({
 	// 	'Cache-Control': 'public,max-age=300'
