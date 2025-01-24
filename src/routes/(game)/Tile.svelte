91d4000d3442d201aa6a0c87faa230fdf7eab5c0
Grant Montgomery <lettucebowler@gmail.com>
2025-01-23 20:15:27 -0600

    bleh

--- src/routes/(game)/Tile.svelte
+++ src/routes/(game)/Tile.svelte
@@ -25,7 +25,7 @@
 
 <div
 	style="--tile-column: var(--column, 0); --animation-delay:{column *
-		0.03}s; --transition-delay:{column * 0.03 + 0.15}s"
+		0.03}s; --transition-delay:{column * delayScale + duration}s"
 	class={[
 		'aspect-square rounded-xl pt-0.5 transition delay-(--transition-delay)',
 		!doWiggle && !doWiggleOnce && '[animation-delay:var(--animation-delay)]',
@@ -36,12 +36,14 @@
 		doWiggleOnce && 'animate-wiggle-once',
 		doJump && 'animate-jump'
 	]}
-	data-answer={answer}
 >
 	<div
 		class={[
-			'in-data-[answer=c]:bg-putty-500 in-data-[answer=c]:text-putty-800 in-data-[answer=x]:bg-swamp-green-500 in-data-[answer=x]:text-swamp-green-800 in-data-[answer=i]:bg-charade-700 text-charade-100 relative box-content grid aspect-square items-center rounded-xl text-center text-2xl font-bold transition transition-all delay-(--transition-delay) duration-0 in-data-answer:shadow-[0_var(--tile-height)_4px_0_rgb(0_0_0_/_0.2)] sm:text-3xl',
-			answer === 'x' && 'bg-swamp-green-500 text-swamp-green-800'
+			'text-charade-100 relative box-content grid aspect-square items-center rounded-xl text-center text-2xl font-bold transition transition-all delay-(--transition-delay) duration-0 sm:text-3xl',
+			answer === 'x' && 'bg-swamp-green-500 text-swamp-green-800',
+			answer === 'c' && 'bg-putty-500 text-putty-800',
+			answer === 'i' && 'bg-charade-700',
+			answer && 'shadow-[0_var(--tile-height)_4px_0_rgb(0_0_0_/_0.2)]'
 		]}
 	>
 		<input
